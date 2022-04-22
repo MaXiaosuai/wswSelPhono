88787fccefe16167e4b62bab1739505199fa218d
MaXiaosuai <1002547276@qq.com>
2022-04-22 18:24:26 +0800

    Add files via upload

--- src/ios/wswSelPhono.m
+++ src/ios/wswSelPhono.m
@@ -1,28 +1,51 @@
-/********* wswSelPhono.m Cordova Plugin Implementation *******/
+#import "wswSelPhono.h"
+#import "JFImagePickerController.h"
 
-#import <Cordova/CDV.h>
+@interface wswSelPhono () <JFImagePickerDelegate>
 
-@interface wswSelPhono : CDVPlugin {
-  // Member variables go here.
-}
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@property (nonatomic,strong)NSMutableArray *resultStrings;
 @end
-
 @implementation wswSelPhono
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
+
+- (void)selPhoto:(CDVInvokedUrlCommand *)command
 {
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    NSDictionary *params = [command.arguments objectAtIndex:0];
+       if (![params objectForKey:@"max"]) {
+           [self failWithCallbackID:command.callbackId withMessage:@"参数格式错误"];
+           return ;
+       }
+    self.resultStrings = [NSMutableArray array];
+    JFImagePickerController *picker = [[JFImagePickerController alloc] initWithRootViewController:nil];
+    picker.pickerDelegate = self;
+    [self.viewController presentViewController:picker animated:YES completion:nil];
 }
+- (void)selCamera:(CDVInvokedUrlCommand *)command
+{
+    
+}
+
+- (void)imagePickerDidFinished:(JFImagePickerController *)picker{
+    NSLog(@"%@",picker.assets);
+    [self imagePickerDidCancel:picker];
+}
+
+- (void)imagePickerDidCancel:(JFImagePickerController *)picker{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+- (void)successWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
+{
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+
+
+
+- (void)failWithCallbackID:(NSString *)callbackID withMessage:(NSString *)message
+{
+    CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:message];
+    [self.commandDelegate sendPluginResult:commandResult callbackId:callbackID];
+}
+
 
 @end
